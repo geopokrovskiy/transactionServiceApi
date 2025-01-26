7de6e21581785f87df16ce9e5f576f54a81b13e2
geopokrovskiy <99304283+geopokrovskiy@users.noreply.github.com>
2025-01-26 20:24:59 +0100

    Pessimistic lock

--- transactionServiceApi/src/main/java/com/geopokrovskiy/repository/WalletRepository.java
+++ transactionServiceApi/src/main/java/com/geopokrovskiy/repository/WalletRepository.java
@@ -1,9 +1,15 @@
 package com.geopokrovskiy.repository;
 
 import com.geopokrovskiy.entity.wallet.WalletEntity;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface WalletRepository extends JpaRepository<WalletEntity, UUID> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<WalletEntity> findById(UUID uuid);
 }
